4a7f5d14c45c67b0ada81b5ec7b93273f0448374
christianchiarulli <chrisatmachine@gmail.com>
2022-02-03 19:37:18 -0500

    fix get env vars

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@
 require('@nomiclabs/hardhat-waffle')
 require("dotenv").config();
 
-const RINKEBY_URL = 'process.env.RINKEBY_URL'
-const RINKEBY_PRIVATE_KEY = 'process.env.RINKEBY_PRIVATE_KEY'
+const RINKEBY_URL = process.env.RINKEBY_URL
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
 
 module.exports = {
   solidity: '0.8.4',
