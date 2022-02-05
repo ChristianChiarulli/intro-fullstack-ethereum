352e1608d7b3a77fd2da8602f63eb903268053ca
christianchiarulli <chrisatmachine@gmail.com>
2022-02-04 19:44:44 -0500

    further document code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ const simpleStorageAddress = '0xde4De608C284709E8980212C7A48B8bcA5b570A2'
 const simpleStorageAbi = SimpleStorage.abi
 
 // NOTE: checkout the API for ethers.js here: https://docs.ethers.io/v5/api/
+// TIP: Remember to console.log something if you are unsure of what is being returned
 
 const App = () => {
   const [provider, setProvider] = useState()
@@ -29,7 +30,7 @@ const App = () => {
       // get provider injected by metamask
       const provider = new ethers.providers.Web3Provider(window.ethereum)
 
-      // Set some data like block number and gas price provided
+      // Set some data like block number and gas price provided, you can find more options in the API docs
       const setBlockchainData = async () => {
         setBlockNumber(await provider.getBlockNumber())
         let gasPrice = await provider.getGasPrice()
@@ -45,38 +46,51 @@ const App = () => {
     }
   }, [])
 
-  // handles setting our account
+  // handles setting account and balance
   const accountHandler = async (account) => {
     setAccount(account)
-    const balance = await provider.getBalance(account.toString())
+    const balance = await provider.getBalance(account)
+    // notice that we use format ether here, uncomment the following console.log and see what happens if we don't
+    // console.log(balance)
     setBalance(ethers.utils.formatEther(balance))
   }
 
+  // handles connecting account
   const connectHandler = async () => {
     const accountList = await provider.listAccounts()
     accountHandler(accountList[0])
     setConnected(true)
   }
 
+  // handles submit button
   const handleSubmit = async (e) => {
-    e.preventDefault() // stops page from refreshing
+    // stops page from refreshing
+    e.preventDefault()
+
+    // create instance of contract using our contract address, abi, and provider
     const contract = new ethers.Contract(
       simpleStorageAddress,
       simpleStorageAbi,
       provider
     )
+
+    // a signer is necessary when your want to write to the blockchain
+    // your wallet doesn't need to sign or spend any ether to read from the blockchain
+    // but it does need to spend ether and therefore sign to write to the blockchain
     const signer = provider.getSigner()
     const contractWithSigner = contract.connect(signer)
+    // we can use 'set' here because the abi provides us with a reference to the methods defined in our smart contract
     console.log(await contractWithSigner.set(inputValue))
     console.log(inputValue)
   }
 
-  const handlRetrieveData = async () => {
+  const handleRetrieveData = async () => {
     const simpleStorageContract = new ethers.Contract(
       simpleStorageAddress,
       simpleStorageAbi,
       provider
     )
+    // we can use 'get' here because the abi provides us with a reference to the methods defined in our smart contract
     setValue(ethers.utils.formatUnits(await simpleStorageContract.get(), 0))
   }
 
@@ -120,7 +134,7 @@ const App = () => {
         </div>
         <div className='card'>
           <h2>Get Value</h2>
-          <button onClick={handlRetrieveData}>Retrieve</button>
+          <button onClick={handleRetrieveData}>Retrieve</button>
           <label>{value}</label>
         </div>
       </section>
